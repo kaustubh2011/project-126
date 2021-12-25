979255d55ea6d41ac0a3d7cde4b5e98949530d44
kaustubh2011 <81859002+kaustubh2011@users.noreply.github.com>
2021-12-25 16:27:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 
 
+scorerightwrist=0;
+scoreleftwrist=0;
+rightwristx=0;
+rightwristy=0;
+leftwristx=0;
+leftwristy=0;
 function setup()
 {
     canvas=createCanvas(500,400);
@@ -9,15 +15,33 @@ function setup()
     posenet=ml5.poseNet(video,modelloaded);
     posenet.on('pose',gotposes);  
 }
-function gotposes(results)
-{
-    console.log(results);
-}
 function modelloaded()
 {
     console.log("posenet is started");
 }
+function gotposes(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        scoreleftwrist=results[0].pose.keypoints[9].score;
+        scorerightwrist=results[0].pose.keypoints[10].score;
+        rightwristx=results[0].pose.rightWrist.x;
+        rightwristy=results[0].pose.rightWrist.y;
+        leftwristx=results[0].pose.leftWrist.x;
+        leftwristy=results[0].pose.leftWrist.y;
+        console.log("rightx= "+rightwristx+ "righty= "+rightwristy);
+        console.log("leftx= "+leftwristx+ "lefty= "+leftwristy);
+    }
+}
+
 function draw()
 {
     image(video,0,0,500,400);
+}
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
